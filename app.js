e8c930b53e93a9cb00959d1b5487644fda5ff03c
syndicate <pranitreddyfte@gmail.com>
2023-06-14 21:44:24 -0700

    Fixed 1st Parallax
    Fixed with working first Mouse Parallax , all images move at same speed

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const parallax_el = document.querySelectorAll(".parallax");
 
-let xValue=0,yValue=0; /*tracking image displacement*/
+let xValue = 0,
+  yValue = 0; /*tracking image displacement*/
 
-window.addEventListener("mousemove",(e) =>{
-xValue = e.clientX - window.innerWidth/2; /*event object*/
-yValue = e.clientY - window.innerHeight/2; /*getting centre position of screen*/
-/*mouse is now relative to the centre of the screen*/
-console.log(xValue,yValue); /*shows mouse coordinates*/
+window.addEventListener("mousemove", (e) => {
+  xValue = e.clientX - window.innerWidth / 2; /*event object*/
+  yValue = e.clientY - window.innerHeight / 2; /*getting centre position*/
+  /*mouse is now relative to the centre of the screen*/
 
-parallax_el.forEach((el)=>{
-    el.style.transform = 'translateX(calc(-50% + ${xValue}px))translateY(calc(-50%) + ${yValue}px)';
-});
+  parallax_el.forEach((el) => {
+    el.style.transform = `translateX(calc(-50% + ${xValue}px)) translateY(calc(-50% + ${yValue}px)`;
+  });
 });
