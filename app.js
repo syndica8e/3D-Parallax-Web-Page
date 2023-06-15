7d709236d490eabc051e9e3de87ad80a19a2526a
syndicate <pranitreddyfte@gmail.com>
2023-06-14 22:41:36 -0700

    added Speedz in img

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ window.addEventListener("mousemove", (e) => {
   parallax_el.forEach((el) => {
     let speedx = el.dataset.speedx;
     let speedy = el.dataset.speedy;
+    let speedz = el.dataset.speedz;
+
     let isInLeft =
       parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
     let zValue = (e.clientX - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
@@ -20,6 +22,6 @@ window.addEventListener("mousemove", (e) => {
       -xValue * speedx
     }px)) translateY(calc(-50% + ${
       yValue * speedy
-    }px)) perspective(2300px) translateZ(${zValue}px)`;
+    }px)) perspective(2300px) translateZ(${zValue * speedz}px)`;
   });
 });
