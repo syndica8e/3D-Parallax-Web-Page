4f5b553010e457d0a2bfc45246c0b28dca9e59e8
syndicate <pranitreddyfte@gmail.com>
2023-06-14 22:34:28 -0700

    Fixed L R mountain speed
    slowed down the speed

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ window.addEventListener("mousemove", (e) => {
     let speedy = el.dataset.speedy;
     let isInLeft =
       parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
-    let zValue = (e.clientX - parseFloat(getComputedStyle(el).left)) * isInLeft;
+    let zValue = (e.clientX - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
     el.style.transform = `translateX(calc(-50% + ${
       -xValue * speedx
     }px)) translateY(calc(-50% + ${
