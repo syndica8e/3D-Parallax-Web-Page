2f1a489c4a8da78a2c7c1d7cb172cc4fdb6e06b2
syndicate <pranitreddyfte@gmail.com>
2023-06-14 21:46:45 -0700

    Fixed Layers to move Opp of mouse

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ window.addEventListener("mousemove", (e) => {
   xValue = e.clientX - window.innerWidth / 2; /*event object*/
   yValue = e.clientY - window.innerHeight / 2; /*getting centre position*/
   /*mouse is now relative to the centre of the screen*/
+  console.log(xValue, yValue); /*shows mouse coordinates*/
 
   parallax_el.forEach((el) => {
-    el.style.transform = `translateX(calc(-50% + ${xValue}px)) translateY(calc(-50% + ${yValue}px)`;
+    el.style.transform = `translateX(calc(-50% + ${-xValue}px)) translateY(calc(-50% + ${yValue}px)`;
   });
 });
