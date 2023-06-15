8ece8f491a78989e0e899522458278ad597e7200
syndicate <pranitreddyfte@gmail.com>
2023-06-14 22:58:02 -0700

    Moved into function + fixed Exceptions

--- app.js
+++ app.js
@@ -5,14 +5,7 @@ let xValue = 0,
 
 let rotateDegree = 0;
 
-window.addEventListener("mousemove", (e) => {
-  xValue = e.clientX - window.innerWidth / 2; /*event object*/
-  yValue = e.clientY - window.innerHeight / 2; /*getting centre position*/
-  /*mouse is now relative to the centre of the screen*/
-  console.log(xValue, yValue); /*shows mouse coordinates*/
-
-  rotateDegree = (xValue / (window.innerWidth / 2)) * 20; //rotate value 0 to 20
-
+function update(cursorPosition) {
   parallax_el.forEach((el) => {
     let speedx = el.dataset.speedx;
     let speedy = el.dataset.speedy;
@@ -21,11 +14,25 @@ window.addEventListener("mousemove", (e) => {
 
     let isInLeft =
       parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
-    let zValue = (e.clientX - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
+    let zValue = (cursorPosition - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
+
     el.style.transform = `translateX(calc(-50% + ${
       -xValue * speedx
     }px)) translateY(calc(-50% + ${
       yValue * speedy
     }px)) perspective(2300px) translateZ(${zValue * speedz}px) rotateY(${rotateDegree * rotateSpeed}deg)`;
   });
+}
+
+update(0);
+
+window.addEventListener("mousemove", (e) => {
+  xValue = e.clientX - window.innerWidth / 2; /*event object*/
+  yValue = e.clientY - window.innerHeight / 2; /*getting centre position*/
+  /*mouse is now relative to the centre of the screen*/
+  console.log(xValue, yValue); /*shows mouse coordinates*/
+
+  rotateDegree = (xValue / (window.innerWidth / 2)) * 20; //rotate value 0 to 20
+
+  update(e.clientX);
 });
